3a29fed865f9b2c5e600dcf93f20f0a74c50540f
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-19 21:08:14 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -72,4 +72,21 @@ int main() {
     return 0;
 }
 
+Number Pattern Printing Program:
+
+#include <stdio.h>
+
+int main() {
+  int i,j,n;
+  printf("Enter the number of lines: ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+      for(j=1;j<=i;j++){
+          printf("%d ",i);
+      }
+      printf("\n");
+  }
+    return 0;
+}
+
 
