55fb019d20cc40e09f513e8655ad37dae27791b9
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-19 20:26:53 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -49,5 +49,27 @@ int main() {
     return 0;
 }
 
+Pyramid Pattern with Spaces on Left Side:
+
+
+#include <stdio.h>
+
+int main() {
+    int i,j,n;
+    printf("Enter the number of lines: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if((i+j)<=n){
+                printf(" ");
+            } else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
 
