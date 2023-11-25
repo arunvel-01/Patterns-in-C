27594dd963712bd1463fd449592972234eee6af5
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-25 20:35:23 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -103,6 +103,25 @@ int main() {
     return 0;
 }
 
+Inverted Pyramid Pattern with Spaces:
+
+#include <stdio.h>
+int main() {
+    int i,j,k,n;
+    printf("Enter the number of lines: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 
 
 
