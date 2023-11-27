b796f41780792ddb4b58572eb3405137ef5dcfff
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-27 21:51:47 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -141,5 +141,23 @@ int main() {
     return 0;
 }
 
+Inverted Pyramid star pattern without space:
+
+#include <stdio.h>
+int main() {
+    int i,j,space,rows;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+    for(i=rows;i>=1;i--){
+        for(space=1;space<=rows-i;space++){
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 
