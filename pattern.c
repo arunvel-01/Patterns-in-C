4d5d4ecedcd4924a8f3e1532ca863be492d3f846
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-29 23:05:03 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -188,3 +188,24 @@ int main() {
     return 0;
 }
 
+Pyramid Pattern And Inverted Pyramid Pattern With Asterisks:
+
+#include <stdio.h>
+int main() {
+    int i,j,n;
+    printf("Enter the number of lines: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=n-1;i++){
+        for(j=n-1;j>=i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
