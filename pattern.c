00debd2fabdb28550bb293ba676b84f001e22330
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-11-29 23:08:06 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -201,8 +201,8 @@ int main() {
         }
         printf("\n");
     }
-    for(i=1;i<=n-1;i++){
-        for(j=n-1;j>=i;j--){
+    for(i=n-1;i>=1;i--){
+        for(j=1;j<=i;j++){
             printf("*");
         }
         printf("\n");
