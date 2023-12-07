db5e73a485e2eec8e0f684e57507a78597b7a5d1
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-12-07 20:32:03 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -209,3 +209,19 @@ int main() {
     }
     return 0;
 }
+
+Program to Generate Uppercase Alphabet Triangle:
+
+#include <stdio.h>
+int main() {
+    int i,j,n;
+    printf("Enter the number of lines: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%c ",64+j);
+        }
+        printf("\n");
+        }
+    return 0;
+}
